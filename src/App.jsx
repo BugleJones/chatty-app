ea5e14f6f32e9128fbe505a34b199ebddfbb8998
BugleJones <spencerjhamilton@gmail.com>
2017-10-04 05:08:38 +0000

    Added connection to chatty server

--- src/App.jsx
+++ src/App.jsx
@@ -27,28 +27,38 @@ class App extends Component {
         }
       ]
       }
-      this.onNewMessage = this.onNewMessage.bind(this);
+      // this.onNewMessage = this.onNewMessage.bind(this);
     }
 
-  // componentDidMount() {
-  //   console.log("componentDidMount <App />");
-  //   setTimeout(() => {
-  //     console.log("Simulating incoming message");
-  //     // Add a new message to the list of messages in the data store
-  //     const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-  //     const messages = this.state.messages.concat(newMessage)
-  //     // Update the state of the app component.
-  //     // Calling setState will trigger a call to render() in App and all child components.
-  //     this.setState({messages: messages})
-  //   }, 3000);
+  // load() {
+
   // }
+
+  componentDidMount() {
+    this.socket = new WebSocket('ws://localhost:3001');
+
+    this.socket.onopen = (event) => {
+      console.log('Connected to server');
+    }
+
+    this.socket.onmessage = (event) => {
+      console.log('Inside Component DidMount Function',JSON.parse(event.data));
+      let message = JSON.parse(event.data)
+    }
+
+    this.setState({data: 
+      {messages: this.state.data.messages.concat(JSON.parse(event.data)),
+      currentUser: {name: this.state.data.currentUser.name} }})
+
+  }
+
   onNewMessage(newPost) {
     const newMessage = {
-      id: this.state.newId,
-      username: this.state.currentUser.name,
+      id: this.state.data.newId,
+      username: this.state.data.currentUser.name,
       content: newPost,
     };
-    const updatedMessages = this.state.messages.concat(newMessage)
+    const updatedMessages = this.state.data.messages.concat(newMessage)
     this.setState({
       messages: updatedMessages,
       newId: this.state.newId + 1,
@@ -62,8 +72,8 @@ class App extends Component {
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-          <MessageList messages={ this.state.messages }/>
-          <ChatBar currentUser={ this.state.currentUser } onNewMessage={this.onNewMessage}/>
+          <MessageList messages={ this.state.data.messages }/>
+          <ChatBar currentUser={ this.state.data.currentUser} onNewMessage={this.onNewMessage}/>
       </div>
     );
   }
